5eb08779e0bd27d7fb6758ae38f7609ed0ba0fb7
juanpdoss <juanpdoss@gmail,com>
2020-10-01 20:30:13 -0300

    subo tp2

---
+++ TP2/Entidades/Taller.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public sealed class Taller
+    {
+        #region atributos
+        private List<Vehiculo> vehiculos;
+        private int espacioDisponible;
+        #endregion
+
+        #region enumerados
+        public enum ETipo
+        {
+            Ciclomotor, 
+            Sedan, 
+            Suv, 
+            Todos
+        }
+        #endregion
+
+
+        #region "Constructores"
+        private Taller()
+        {
+            this.vehiculos = new List<Vehiculo>();
+        }
+
+
+        public Taller(int espacioDisponible) : this()
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas de metodos"
+        /// <summary>
+        /// Muestro el estacionamiento y TODOS los vehículos
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Taller.Listar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="taller">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        public static string Listar(Taller taller, ETipo tipo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} disponibles", taller.vehiculos.Count, taller.espacioDisponible);
+            sb.AppendLine("");
+
+            foreach (Vehiculo auxiliar in taller.vehiculos)
+            {
+                switch (tipo)
+                {
+                    //en cada case valido con operador IS que el vehiculo sea del tipo requerido para agregar sus datos al StringBuilder
+                    case ETipo.Suv:
+                        if(auxiliar is Suv)
+                        {
+                            sb.AppendLine(auxiliar.Mostrar());  
+                        }                                                                       
+                        break;
+
+                    case ETipo.Ciclomotor:
+                        if(auxiliar is Ciclomotor)
+                        {
+                            sb.AppendLine(auxiliar.Mostrar());
+                        }                       
+                        break;
+
+                    case ETipo.Sedan:
+                        if(auxiliar is Sedan)
+                        {
+                            sb.AppendLine(auxiliar.Mostrar());
+                        }                    
+                        break;
+                    
+                    default:
+                        {
+                            sb.AppendLine(auxiliar.Mostrar());
+                        }                       
+                        break;
+                   
+                }
+            }
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista, validando que el elemento no este en la lista ni que se exceda la capacidad de la misma.
+        /// </summary>
+        /// <param name="taller">Objeto donde se agregará el elemento</param>
+        /// <param name="vehiculo">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Taller operator +(Taller taller, Vehiculo vehiculo)
+        {
+            bool yaEstaEnTaller = false;
+            int contadorLugares = 0;
+
+            foreach(Vehiculo auxiliar in taller.vehiculos)
+            {
+                //itero cada vehiculo en la lista de vehiculos
+                //si el vehiculo que recibo por parametro esta en el taller, 
+                //corto la estructura repetitiva y cambio mi variable yaEstaEnTaller a true
+
+                if(auxiliar.Equals(vehiculo))
+                {
+                    yaEstaEnTaller = true;
+                    break;
+                }
+                contadorLugares++;
+            }
+
+            //si ya esta en el taller, o si el contador de lugares no es menor al espacio espacio disponible, no agrego el vehiculo.
+            if(!yaEstaEnTaller && contadorLugares<taller.espacioDisponible)
+            {
+                taller.vehiculos.Add(vehiculo);
+            }
+
+            return taller;
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="taller">Objeto donde se quitará el elemento</param>
+        /// <param name="vehiculo">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Taller operator -(Taller taller, Vehiculo vehiculo)
+        {
+            if(taller.vehiculos.Contains(vehiculo))
+            {
+                taller.vehiculos.Remove(vehiculo);
+            }
+
+            return taller;
+        }
+        #endregion
+    }
+}
---
+++ TP2/Entidades/Vehiculo.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        #region enumerados
+
+        public enum EMarca
+        {
+            Chevrolet, 
+            Ford, 
+            Renault, 
+            Toyota, 
+            BMW, 
+            Honda, 
+            HarleyDavidson
+        }
+
+        public enum ETamanio
+        {
+            Chico, 
+            Mediano, 
+            Grande
+        }
+
+        #endregion
+
+        #region atributos   
+
+        private EMarca marca;
+        private string chasis;
+        private ConsoleColor color;
+
+        #endregion
+
+        #region constructor
+        public Vehiculo(EMarca marca, string chasis, ConsoleColor color)
+        {
+            this.marca = marca;
+            this.chasis = chasis;
+            this.color = color;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        /// 
+
+        #region propiedades
+        protected virtual ETamanio Tamanio
+        {           
+            get
+            {
+                return this.Tamanio;
+            }
+                
+        }
+
+        #endregion
+
+        #region metodos
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+        
+        public static explicit operator string(Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("CHASIS: {0}\r\n", p.chasis);
+            sb.AppendFormat("MARCA : {0}\r\n", p.marca.ToString());
+            sb.AppendFormat("COLOR : {0}\r\n", p.color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+
+        #region sobrecarga de operadores
+
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            bool sonIguales = false;
+
+            //comparo por atributo chasis
+            if(v1.chasis == v2.chasis)
+            {
+                sonIguales = true;
+            }
+
+            return sonIguales;
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            //reutilizo el operador ==
+            return !(v1.chasis == v2.chasis);
+        }
+        #endregion
+
+        public override bool Equals(object obj)
+        {
+            bool sonIguales = false;
+
+            //reutilizo el operador ==, por lo que internamente sigo comparado por atributo chasis
+        
+            if(obj is Vehiculo)
+            {
+                sonIguales = this == (Vehiculo)obj;
+            }
+       
+            return sonIguales;    
+        }
+
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+
+    }
+}
