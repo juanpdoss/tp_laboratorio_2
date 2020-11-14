3a68f6c05689652a745ccec07038f7528ce58a3f
juanpdoss <juanpdoss@gmail,com>
2020-11-14 15:04:22 -0300

    tp1

---
+++ recuperatoriosTP/TP1/Entidades/Numero.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// Clase instanciable Numero
+    /// </summary>
+    public class Numero
+    {
+        #region atributos
+        private double numero;
+        #endregion
+
+        #region propiedades
+        /// <summary>
+        /// Propiedad de solo escritura que asigna el atributo numero previa validacion.
+        /// </summary>
+        public string SetNumero
+        {
+            set
+            {
+                this.numero = ValidarNumero(value);
+            }
+        }
+        #endregion
+
+        #region constructores 
+        //constructor de instancia por defecto.
+        public Numero()
+        {
+
+        }
+
+        //Constructor de instancia, recibe un double como parametro.
+        public Numero(double numero) : this(numero.ToString())
+        {
+
+        }
+
+        //constructor de instancia, recibe un string como parametro.
+        public Numero(string strNumero)
+        {
+
+            this.SetNumero = strNumero;
+        }
+        #endregion
+
+        #region metodos 
+        /// <summary>
+        /// Valida que la cadena recibida como parametro sea un numero binario y lo retorna en formato decimal,
+        /// caso contrario retornara "valor invalido".
+        /// </summary>
+        /// <param name="binario" cadena que contiene el numero en binario></param>
+        /// <returns></returns>
+        public string BinarioDecimal(string binario)
+        {
+            int numero = 0;
+            string retorno = "Valor invalido";
+
+            if (EsBinario(binario))
+            {
+                numero = Convert.ToInt32(binario, 2);
+                retorno = Convert.ToString(numero);
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Convierte, en caso de ser posible, el numero decimal recibido como parametro a su representacion en binario
+        /// </summary>
+        /// <param name="numero" numero en base decimal></param>
+        /// <returns></returns>
+        public string DecimalBinario(double numero)
+        {
+            string retorno = "Valor invalido";
+            if (numero > 0)
+            {
+                retorno = Convert.ToString((int)numero, 2);
+            }
+
+            return retorno;
+
+        }
+        /// <summary>
+        /// Convierte, en caso de ser posible, el numero decimal contenido en la cadena recibida como parametro
+        /// a su representacion en binario
+        /// </summary>
+        /// <param name="numero" cadena que contiene el numero></param>
+        /// <returns></returns>
+        public string DecimalBinario(string numero)
+        {
+
+            string respuesta = "Valor invalido";
+            double auxiliar;
+
+            if (double.TryParse(numero, out auxiliar))
+            {
+                respuesta = DecimalBinario(auxiliar);
+            }
+
+
+            return respuesta;
+
+        }
+        /// <summary>
+        /// valida que la cadena recibida esta compuesta solo por unos y ceros, caso contrario se retornara false.
+        /// </summary>
+        /// <param name="binario" cadena que contiene el numero en binario></param>
+        /// <returns></returns>
+        private bool EsBinario(string binario)
+        {
+            bool retorno = false;
+            int cont = 0;
+            foreach (char aux in binario)
+            {
+
+                if (aux == '1' || aux == '0')
+                {
+                    retorno = true;
+                    cont++;
+                }
+
+            }
+
+            if (cont != binario.Length)
+            {
+                retorno = false;
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Valida que la cadena recibida como parametro sea un valor numerico, caso contrario se retornara cero.
+        /// </summary>
+        /// <param name="strNumero"></param>
+        /// <returns></returns>
+        private static double ValidarNumero(string strNumero)
+        {
+            double numeroValidado;
+
+            if (!double.TryParse(strNumero, out numeroValidado))
+            {
+                numeroValidado = 0;
+            }
+
+
+            return numeroValidado;
+
+        }
+        #endregion
+
+        #region sobrecarga de operadores 
+        /// <summary>
+        /// retorna la diferencia de los campos numero de dos objetos Numero.
+        /// </summary>
+        /// <param name="num1" objeto de tipo Numero ></param>
+        /// <param name="num2" objeto de tipo Numero></param>
+        /// <returns></returns>
+        public static double operator -(Numero num1, Numero num2)
+        {
+            return num1.numero - num2.numero;
+        }
+
+        /// <summary>
+        /// retorna la suma de los campo numero de dos objetos Numero.
+        /// </summary>
+        /// <param name="num1" objeto de tipo Numero></param>
+        /// <param name="num2" objeto de tipo Numero></param>
+        /// <returns></returns>
+        public static double operator +(Numero num1, Numero num2)
+        {
+
+            return num1.numero + num2.numero;
+        }
+
+        /// <summary>
+        /// retorna el producto de los campos numero de dos objetos Numero.
+        /// </summary>
+        /// <param name="num1" objeto de tipo Numero ></param>
+        /// <param name="num2" objeto de tipo Numero ></param>
+        /// <returns></returns>
+
+        public static double operator *(Numero num1, Numero num2)
+        {
+            return num1.numero * num2.numero;
+        }
+
+        /// <summary>
+        /// retorna la division de los campos numero de dos objetos Numero, en caso de que num2 sea cero, retornara double.MinValue.
+        /// </summary>
+        /// <param name="num1" objeto de tipo Numero ></param>
+        /// <param name="num2" objeto de tipo Numero ></param>
+        /// <returns></returns>
+        public static double operator /(Numero num1, Numero num2)
+        {
+            double resultado = double.MinValue;
+
+            if (num2.numero != 0)
+            {
+                resultado = num1.numero / num2.numero;
+            }
+
+            return resultado;
+
+        }
+
+
+
+        #endregion
+
+    }
+}
--- recuperatoriosTP/TP1/Entidades/Properties/AssemblyInfo.cs
+++ recuperatoriosTP/TP1/Entidades/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("MiCalculadora")]
+[assembly: AssemblyTitle("Entidades")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiCalculadora")]
+[assembly: AssemblyProduct("Entidades")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("d32ae13c-c0ab-4cfd-b793-4d11ed368754")]
+[assembly: Guid("f1000b6e-5b81-4f2c-ade6-60e2d9788c4e")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
